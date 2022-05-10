e3da77926d80c0f879a0ff3c87185ed06f5cff29
Joan Nicolau <joannicolau23@gmail.com>
2022-05-10 22:36:30 +0200

    add repository boilerplate

---
+++ src/main/java/edu/poniperro/repositories/GreetingRepository.java
@@ -0,0 +1,8 @@
+package edu.poniperro.repositories;
+
+import edu.poniperro.domain.Greeting;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class GreetingRepository implements PanacheRepository<Greeting> {
+    
+}
