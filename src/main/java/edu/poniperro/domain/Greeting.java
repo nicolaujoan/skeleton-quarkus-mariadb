f643d4e4b4c18bce61d88c48fb29f23804aab37c
Joan Nicolau <joannicolau23@gmail.com>
2022-05-10 22:35:56 +0200

    add domain boilerplate

---
+++ src/main/java/edu/poniperro/domain/Greeting.java
@@ -0,0 +1,10 @@
+package edu.poniperro.domain;
+
+/**
+ * in case using active record pattern -> delete repositories folder and
+ * extend every entity from PanacheEntity
+ */
+
+public class Greeting {
+    
+}
