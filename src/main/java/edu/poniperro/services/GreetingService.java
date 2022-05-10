28af39e52fd0c8c436c7d49d5cd819cae21179f1
Joan Nicolau <joannicolau23@gmail.com>
2022-05-10 22:37:01 +0200

    add services boilerplate

---
+++ src/main/java/edu/poniperro/services/GreetingService.java
@@ -0,0 +1,8 @@
+package edu.poniperro.services;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+    public String greeting = "hello from GreetingService!";
+}
