5d3e7822100a6c3c7771c11496cd082a2eb21576
Joan Nicolau <joannicolau23@gmail.com>
2022-05-10 22:39:27 +0200

    inject service

--- src/main/java/edu/poniperro/GreetingResource.java
+++ src/main/java/edu/poniperro/GreetingResource.java
@@ -1,16 +1,22 @@
 package edu.poniperro;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import edu.poniperro.services.GreetingService;
+
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    GreetingService greetingService;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return greetingService.greeting;
     }
 }
